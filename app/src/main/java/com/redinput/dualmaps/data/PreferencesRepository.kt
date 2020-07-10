c1af2408b4724a3763b7d7099c972ca7543a0c76
Ismael Reyes <ismael.kix2902@gmail.com>
2020-07-10 20:49:06 +0200

    Updated preference repository to load preferences values

--- app/src/main/java/com/redinput/dualmaps/data/PreferencesRepository.kt
+++ app/src/main/java/com/redinput/dualmaps/data/PreferencesRepository.kt
@@ -2,6 +2,7 @@ package com.redinput.dualmaps.data
 
 import android.content.Context
 import androidx.preference.PreferenceManager
+import com.redinput.dualmaps.R
 import com.redinput.dualmaps.helpers.SingletonHolder
 
 class PreferencesRepository private constructor(context: Context) : PreferencesSource {
@@ -9,6 +10,15 @@ class PreferencesRepository private constructor(context: Context) : PreferencesS
     companion object : SingletonHolder<PreferencesRepository, Context>(::PreferencesRepository) {
         private const val KEY_GDPR_ENABLED = "gdpr-enabled"
     }
+
+    private val KEY_MAP_TYPE = context.getString(R.string.key_map_type)
+    private val DEFAULT_MAP_TYPE = context.getString(R.string.default_map_type)
+
+    private val KEY_SHOW_COMPASS = context.getString(R.string.key_show_compass)
+    private val DEFAULT_SHOW_COMPASS = context.resources.getBoolean(R.bool.default_show_compass)
+
+    private val KEY_SHOW_ADDRESS = context.getString(R.string.key_show_address)
+    private val DEFAULT_SHOW_ADDRESS = context.resources.getBoolean(R.bool.default_show_address)
 
     private val preferences = PreferenceManager.getDefaultSharedPreferences(context)
 
@@ -18,5 +28,17 @@ class PreferencesRepository private constructor(context: Context) : PreferencesS
 
     override fun saveFirebaseEnabled(enabled: Boolean) {
         preferences.edit().putBoolean(KEY_GDPR_ENABLED, enabled).apply()
+    }
+
+    override fun loadMapType(): Int {
+        return preferences.getString(KEY_MAP_TYPE, DEFAULT_MAP_TYPE)!!.toInt()
+    }
+
+    override fun loadShowCompass(): Boolean {
+        return preferences.getBoolean(KEY_SHOW_COMPASS, DEFAULT_SHOW_COMPASS)
+    }
+
+    override fun loadShowAddress(): Boolean {
+        return preferences.getBoolean(KEY_SHOW_ADDRESS, DEFAULT_SHOW_ADDRESS)
     }
 }
--- app/src/main/java/com/redinput/dualmaps/data/PreferencesSource.kt
+++ app/src/main/java/com/redinput/dualmaps/data/PreferencesSource.kt
@@ -3,4 +3,7 @@ package com.redinput.dualmaps.data
 interface PreferencesSource {
     fun loadFirebaseEnabled(): Boolean
     fun saveFirebaseEnabled(enabled: Boolean)
+    fun loadMapType(): Int
+    fun loadShowCompass(): Boolean
+    fun loadShowAddress(): Boolean
 }
